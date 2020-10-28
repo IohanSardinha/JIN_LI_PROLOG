38dbfea1ffbde75d487e547917701fc71a7aabff
IohanSardinha <ioxsardinha@gmail.com>
2020-10-28 17:58:05 +0000

    In middle of report

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# JIN LI
+# PLOG - Avaliação Intercalar
 
-A version of the boardgame [Jin Li](https://nestorgames.com/#jinli_detail) made in Prolog
-[Relatório de projeto](https://docs.google.com/document/d/13t-Nrd_4-GkjW65-MXrEBpdyIazhUr6x2qNUO8KImO0/edit?usp=sharing)
+Grupo: JIN_LI_5
+Turma: 5
+Iohan Xavier Sardinha Dutra Soares (up201801011)
+João Diogo Vila Franca Gonçalves (up201806162)
+
+### Descrição do Jogo
+
+Cada jogador possui duas carpas e dez pedras. Um par de carpas vermelho outro amarelo.
+O Objetivo do jogo é fazer dez pontos, fazendo com que suas carpas tenham o máximo de contato umas com as outras.
+O tabuleiro do jogo tem formato quadrado, com 7x7 casas.
+Os jogadores começam com suas carpas nas extremidades do tabuleiro: Em cima as duas vermelhas uma na extrimidade esquerda e outra na direita, e da mesma forma, em baixo, as amarelas.
+Os jogadores intercalam jogadas.
+Em cada jogada podem se mover de duas formas:
+
+- **Nadar**: Onde se move uma casa em qualquer direção, diagonal ou lateral, e após nadar põe uma pedra em qualquer casa vazia do tabuleiro. Se já tiver posto todas as dez pedras não se põe nenhuma.
+- **Saltar**: O jogador pode saltar por cima de uma pedra adjacente, também em qualquer direção, aterrisando numa casa vazia do outro lado. As carpas só conseguem saltar uma pedra de cada vez e após fazê-lo o jogador **não** pode por uma nova pedra no tabuleiro, diferente de quando nada.
+
+Ao final de cada jogada o jogador que se moveu ganha pontos de acordo com a quantidade de carpas adjecentes a carpa que se acabou de se mover, independente da cor do outro peixe.
+O jogo acaba assim que um jogador acumular dez pontos.
+
+### Representação interna do Estado do Jogo
+
+### Visualização Do Estado do Jogo
--- game.pl
+++ game.pl
@@ -1,5 +1,6 @@
-/*
-initialBoard([
+%initial(-Board)
+%Initial board state
+initial([
 [red,empty,empty,empty,empty,empty,red],
 [empty,empty,empty,empty,empty,empty,empty],
 [empty,empty,empty,empty,empty,empty,empty],
@@ -8,31 +9,40 @@ initialBoard([
 [empty,empty,empty,empty,empty,empty,empty],
 [yellow,empty,empty,empty,empty,empty,yellow]
 ]).
+
+%middleBoard(-Board)
+%Representation of a middle game board
 middleBoard([
 [empty,empty,empty,empty,empty,empty,empty],
 [stone,empty,stone,empty,red,stone,empty],
-[empty,empty,red,empty,stone,empty,empty],
-[empty,red,empty,empty,yellow,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty],
+[empty,empty,red,empty,stone,empty,stone],
+[empty,stone,empty,empty,yellow,empty,empty],
+[empty,empty,empty,stone,empty,empty,empty],
 [empty,stone,yellow,empty,empty,empty,empty],
 [empty,empty,empty,empty,stone,empty,empty]
 ]).
 
+%finalBoard(-Board)
+%Representation of a won game board
 finalBoard([
-[empty,empty,empty,empty,stone,empty,empty],
+[empty,empty,stone,empty,stone,empty,empty],
 [stone,empty,stone,empty,empty,stone,empty],
 [empty,stone,stone,empty,stone,empty,empty],
-[empty,stone,empty,empty,stone,red,empty],
-[empty,empty,empty,empty,empty,empty,red],
-[empty,stone,yellow,stone,yellow,stone,empty],
+[empty,stone,empty,stone,stone,red,stone],
+[empty,stone,empty,empty,empty,empty,red],
+[stone,stone,yellow,stone,yellow,stone,empty],
 [empty,stone,empty,empty,stone,stone,empty]
 ]).
 
+%symbol(+state,-symbol).
+%Symbol to be printed from each board cell state
 symbol(empty,S) :- S=' '.
 symbol(yellow,S) :- S='Y'.
 symbol(stone,S) :- S='O'.
 symbol(red,S) :- S='R'.
 
+%letter(+Number, -Letter).
+%Letter to be printed for each board row
 letter(1, L) :- L='A'.
 letter(2, L) :- L='B'.
 letter(3, L) :- L='C'.
@@ -41,15 +51,24 @@ letter(5, L) :- L='E'.
 letter(6, L) :- L='F'.
 letter(7, L) :- L='G'.
 
+%turn(+Player, -Color)
+%Color of each player for display
+turn(0,'Red').
+turn(1,'Yellow').
 
-printBoard(X) :-
+%displayGame(+GameState,+Player)
+%Display the given state of the game and the current turn player
+displayGame(GameState,Player) :-
     nl,
+    turn(Player, P),
+    format("Turn: ~w~n",P),
     write('   | 1 | 2 | 3 | 4 | 5 | 6 | 7 |\n'),
     write('---|---|---|---|---|---|---|---|\n'),
-    printLines(X, 1).
+    printLines(GameState, 1).
 
+%printLines(+GameState, +Line)
+%Prints the Nth line of the Board
 printLines([], 8).
-
 printLines([Head|Tail], N) :-
     letter(N, L),
     write(' '),
@@ -60,7 +79,8 @@ printLines([Head|Tail], N) :-
     write('\n---|---|---|---|---|---|---|---|\n'),
     printLines(Tail, N1).
 
-
+%printObjects(+Line)
+%Prints the symbols in a line of the board
 printObjects([]).
 printObjects([Head|Tail]) :-
     symbol(Head, S),
@@ -68,4 +88,6 @@ printObjects([Head|Tail]) :-
     write(' | '),
     printObjects(Tail).
 
-*/
+%play()
+%Shows the initial state of the game
+play(_) :- initial(Board),turn(FirstPlayer,'Yellow'), displayGame(Board,FirstPlayer).
