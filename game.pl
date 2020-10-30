e88b56562fef1ca140287085181a66a9b714925c
IohanSardinha <ioxsardinha@gmail.com>
2020-10-30 21:33:05 +0000

    Small changes

--- game.pl
+++ game.pl
@@ -53,14 +53,38 @@ letter(7, L) :- L='G'.
 
 %turn(+Player, -Color)
 %Color of each player for display
-turn(0,'Red').
-turn(1,'Yellow').
+turn(red,'Red').
+turn(yellow,'Yellow').
+
+%score(+Player, -score)
+%Gets each player score
+score(red, 0).
+score(yellow, 0).
+
+%addScore(+Player,+Increment)
+%Adds Increment points to player Player
+%addScore(Player,N) :- drop(Player,_), push(Player,N).
+
+%stones(+Player, -NumStones)
+%Gets each player stones left
+stones(red,10).
+stones(yellow, 10).
+
+%removeStone(+Player)
+%Removes a stone from player count
+%removeStone(+Player) :- drop(+Player, N), N1 is N -1, push(+Player, N1).
 
 %displayGame(+GameState,+Player)
 %Display the given state of the game and the current turn player
 displayGame(GameState,Player) :-
     turn(Player, P),
+    score(red,RedScore),
+    score(yellow,YellowScore),
+    stones(Player,Stones),
     format("Turn: ~w~n",P),
+    format("Stones: ~w~n",Stones),
+    format("Score: ~n",[]),
+    format("    Red: ~w     Yellow: ~w~n",[RedScore,YellowScore]),
     write('   | 1 | 2 | 3 | 4 | 5 | 6 | 7 |\n'),
     write('---|---|---|---|---|---|---|---|\n'),
     printLines(GameState, 1).
@@ -89,4 +113,4 @@ printObjects([Head|Tail]) :-
 
 %play()
 %Shows the initial state of the game
-play(_) :- initial(Board),turn(FirstPlayer,'Yellow'), displayGame(Board,FirstPlayer).
+play(_) :- initial(Board), displayGame(Board,yellow).
